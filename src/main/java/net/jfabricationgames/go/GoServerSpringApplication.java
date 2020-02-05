faa2d6c8e2528d4129d8c113912dc96d8a930fa3
tobias <tobias-fassbender@gmx.de>
2020-02-05 10:59:42 +0100

    initial commit: started go server spring project

---
+++ src/main/java/net/jfabricationgames/go/GoServerSpringApplication.java
@@ -0,0 +1,12 @@
+package net.jfabricationgames.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoServerSpringApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GoServerSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/net/jfabricationgames/go/GoServerSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package net.jfabricationgames.go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoServerSpringApplicationTests {
+	
+	@Test
+	void contextLoads() {}
+}
