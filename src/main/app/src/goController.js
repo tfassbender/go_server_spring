4d48e34ef805c1c0216aa0c0a5f6c1354d0d0525
tobias <tobias-fassbender@gmx.de>
2020-02-24 18:38:15 +0100

    added controller buttons to the react test app

--- src/main/app/src/goController.js
+++ src/main/app/src/goController.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 export default function GoControllerComponent() {
+	
+	const [id, setId] = React.useState(0);
+	
+	function createGame() {
+		alert("Creating game: " + id);
+	}
+	
+	function loadGame() {
+		alert("Loading game: " + id);
+	}
+	
+	function deleteGame() {
+		alert("Deleting game: " + id);
+	}
+	
 	return <div>
-		<p>TODO Go Controller</p>
+		<table>
+			<tr>
+				<td><label htmlFor="id">Game ID:</label></td>
+				<td><input type="number" value={id} onInput={e => setId(e.target.value)} /></td>
+			</tr>
+			<tr>
+				<td><button onClick={createGame}>Create Game</button></td>
+			</tr>
+			<tr>
+				<td><button onClick={loadGame}>Load Game</button></td>
+			</tr>
+			<tr>
+				<td><button onClick={deleteGame}>Delete Game</button></td>
+			</tr>
+		</table>
 	</div>;
 }
